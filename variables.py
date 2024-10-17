544fd9d2b31a70f148f882a064b54fb76a63d4d1
24122024 <arakelyantssasha@gmail.com>
2024-10-17 17:54:41 +0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,12 @@
+course = 'Python' # ПЕРЕМЕННЫЕ
+print(course)
+total_tasks = ('a') # всего задач
+a = 12
+print(a)
+hours_spent = ('b') # затрачено часов
+b = 1.5
+print(b)
+average_execution_time = ('c') # среднее время выполнения
+c = b/a
+print(c)
+print('Course: Python,' 'total_tasks:12,' 'hours_spent:1.5,' 'average_eaxecution_time 0.125 hours.')
